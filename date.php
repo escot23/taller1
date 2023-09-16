f4de864724f7b31eef8a7fe4286191b5c4e603fd
escot23 <127620625+escot23@users.noreply.github.com>
2023-09-16 13:08:01 -0600

    Add files via upload

---
+++ date.php
@@ -0,0 +1,9 @@
+<?php
+            //AÑO-MES- DIA
+			//HORA,MINUTO,SEGUNDO
+$Date = date("Y-m-d H:i:s"); // Obtener la fecha y hora actual en el formato deseado
+
+// Mostrar la fecha y hora actual
+echo "<p>La fecha y hora actual es: $Date</p>";
+?>
+
